9175245e4e139a70757b70b85461499b9834dbcb
dattapatil8 <dattapatil862002@gmail.com>
2026-08-12 14:18:11 +0530

    Add CRUD notes API

---
+++ CrudeNote/src/app.js
@@ -0,0 +1,45 @@
+const express=require("express")
+
+
+
+const app= express()
+app.use(express.json())
+
+const notes=[]
+
+app.post("/notes",(req,res)=>{
+  notes.push(req.body)
+
+  res.status(201).json({
+    message:"Note Create Successfully"
+  })
+  
+})
+app.get("/notes",(req,res)=>{
+    res.status(201).json({
+        message :"Note fetching successfully",
+
+        notes :notes
+    })
+  })
+
+  app.delete("/notes/:index",(req,res)=>{
+    const index=req.params.index
+    delete notes[index]
+    res.status(201).json({
+     message :"Note delete successfully"
+    })
+  })
+
+  app.patch("/notes/:index",(req,res)=>{
+const index =req.params.index
+const title=req.body.title
+
+notes[index].title=title
+res.status(200).json({
+    message : "Note Update"
+})
+
+  })
+
+module.exports = app 
